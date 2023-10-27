4406fbeb7f90c97408a7eac254d9121f52f8b442
shoegaev <kshoegaev@gmail.com>
2023-10-27 15:29:51 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    const number = n.toString()
+    let i = number.length - 1
+
+    let result = ''
+
+    while (i >= 0) {
+        if (number[i] === '-') {
+            result = result
+        } else {
+            result = result + number[i]
+        }
+        i = i - 1
+    }
+
+    return Number(result)
 }
